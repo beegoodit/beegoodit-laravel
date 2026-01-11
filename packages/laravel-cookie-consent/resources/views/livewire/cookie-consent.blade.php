4c45cf106de5c5552662aeb47a22b2b4878c412b
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2026-01-11 22:07:25 +0100

    fix(laravel-cookie-consent): settings modal visibility

--- packages/laravel-cookie-consent/resources/views/livewire/cookie-consent.blade.php
+++ packages/laravel-cookie-consent/resources/views/livewire/cookie-consent.blade.php
@@ -209,5 +209,5 @@
         x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0"
         x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-150"
         x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-        class="fixed inset-0 bg-black/60 backdrop-blur-sm z-[9999]" @click="$wire.closeSettings()"></div>
+        class="fixed inset-0 bg-black/60 backdrop-blur-sm z-[9998]" @click="$wire.closeSettings()"></div>
 </div>
