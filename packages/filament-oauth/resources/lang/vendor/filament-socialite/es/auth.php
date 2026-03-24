0f5f77049e530d05c817cf03e826d0ad2e324cc3
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2026-03-24 01:40:32 +0100

    fix(filament-oauth): missing spanish translations

---
+++ packages/filament-oauth/resources/lang/vendor/filament-socialite/es/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'login-via' => 'O inicia sesión con',
+    'login-failed' => 'Error al iniciar sesión, por favor inténtalo de nuevo.',
+    'user-not-allowed' => 'Tu correo no pertenece a un dominio permitido.',
+    'registration-not-enabled' => 'No está permitido registrar nuevos usuarios.',
+];
